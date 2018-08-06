87fa1d9f14a0405d0bf4e6c4826a59c23ecb4e83
dbeley <david.beley@netc.fr>
2018-08-06 09:38:21 +0200

    corrections diverses

--- launch.sh
+++ launch.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # This script will provide an easy way to launch all the scripts placed in the same folder
 
-script="$(ls ~/scripts/*.sh | grep -v "launch.sh" | rofi -dmenu -p "Script " -l 5 -width 30 -no-custom -i)"
+script="$(ls $HOME/scripts/*.sh | xargs -n1 basename | grep -v "launch.sh" | rofi -dmenu -p "Script
+" -l 5 -width 30 -no-custom -i)"
 [ -z "$script" ] && exit 1
 
-bash $script
+bash $HOME/scripts/$script
 
 exit 0
--- search.sh
+++ search.sh
@@ -5,17 +5,17 @@
 
 pgrep -x rofi && exit 1
 
-query="$(cat ~/scripts/searchengines.txt | awk -F ';' '{print $2,"- "$1}' | rofi -p "Recherche " -i -dmenu)"
+query="$(cat $HOME/scripts/searchengines.txt | awk -F ';' '{print $2,"- "$1}' | rofi -p "Recherche " -i -dmenu)"
 [ -z "$query" ] && exit 1
 
-engines="$(cat ~/scripts/searchengines.txt | awk -F ';' '{print $1}')"
+engines="$(cat $HOME/scripts/searchengines.txt | awk -F ';' '{print $1}')"
 
 engine="$(echo $query | awk '{print $1}')"
 
 echo "$engines" | grep  -q  "$engine"
 # if engine is in engines
 if [ $? -eq 0 ];then
-    link="$(cat ~/scripts/searchengines.txt | awk -v e="$engine" -F ';' '$1 == e {print $3}')"
+    link="$(cat $HOME/scripts/searchengines.txt | awk -v e="$engine" -F ';' '$1 == e {print $3}')"
     
     # remove the first element of query
     query="$(echo $query | awk '{for (i=2; i<=NF; i++) printf("%s",( (i>2) ? OFS : "") $i)}')"
