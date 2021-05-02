994dc55199c4a7a11c9267ade8bd59122ac85e0a
dbeley <6568955+dbeley@users.noreply.github.com>
2021-05-03 01:32:10 +0200

    Add monitor number to display_config_dmenu.sh.

--- display_config_dmenu.sh
+++ display_config_dmenu.sh
@@ -2,10 +2,12 @@
 # This script will launch any scripts in the ~/dotfiles/Autres/Écrans/ directory.
 # The scripts in that folder are generated with arandr.
 
+MON=$1
+
 # Import the colors
 . "${HOME}/.cache/wal/colors.sh"
 
-script="$(ls $HOME/dotfiles/Autres/Écrans/*.sh | xargs -n1 basename | dmenu -i -l 10 -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15")"
+script="$(ls $HOME/dotfiles/Autres/Écrans/*.sh | xargs -n1 basename | dmenu -i -l 10 -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" -m $MON)"
 [ -z "$script" ] && exit 1
 
 bash $HOME/dotfiles/Autres/Écrans/$script
