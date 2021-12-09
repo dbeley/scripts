f593498e68b5a38b7e047da7c754964f790a1368
dbeley <6568955+dbeley@users.noreply.github.com>
2021-12-09 18:44:19 +0100

    Add touchpad_toggle_wayland.sh

--- change_layout_wayland.sh
+++ change_layout_wayland.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-swaymsg input type:keyboard xkb_switch_layout prev
-layout=$(swaymsg -t get_inputs | jq '.[0].xkb_active_layout_name')
-notify-send -t 1000 "$layout" -h string:x-canonical-private-synchronous:change_layout_wayland
+swaymsg input type:keyboard xkb_switch_layout next
+LAYOUT=$(swaymsg -t get_inputs | jq '.[0].xkb_active_layout_name')
+notify-send -t 1000 "$LAYOUT" -h string:x-canonical-private-synchronous:change_layout_wayland
---
+++ touchpad_toggle_wayland.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This script will toggle the touchpad (wayland version).
+
+TOUCHPAD_IDENTIFIER=`swaymsg -t get_inputs | jq -r '.[] | select (.type == "touchpad") | .identifier'`
+TOUCHPAD_STATUS=`swaymsg -t get_inputs | jq -r '.[] | select (.type == "touchpad") | .libinput.send_events'`
+
+if [ "$TOUCHPAD_STATUS" == "enabled" ]
+then
+	swaymsg input "$TOUCHPAD_IDENTIFIER" events disabled
+    notify-send -a 'Touchpad' 'Touchpad disabled' -i /usr/share/icons/Adwaita/48x48/devices/input-touchpad.png
+else
+	swaymsg input "$TOUCHPAD_IDENTIFIER" events enabled
+    notify-send -a 'Touchpad' 'Touchpad enabled' -i /usr/share/icons/Adwaita/48x48/devices/input-touchpad.png
+fi
