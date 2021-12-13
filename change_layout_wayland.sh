34046d002a1bc3f242e1a663bd8ee0416b006aef
dbeley <6568955+dbeley@users.noreply.github.com>
2021-12-13 17:15:49 +0100

    Update change_layout_wayland.sh

--- change_layout_wayland.sh
+++ change_layout_wayland.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-swaymsg input type:keyboard xkb_switch_layout next
+swaymsg input "1:1:AT_Translated_Set_2_keyboard" xkb_switch_layout next
 LAYOUT=$(swaymsg -t get_inputs | jq '.[0].xkb_active_layout_name')
 notify-send -t 1000 "$LAYOUT" -h string:x-canonical-private-synchronous:change_layout_wayland
