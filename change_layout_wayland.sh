dc8d08c4715f0782276a831d3dd2cedccc40c000
dbeley <6568955+dbeley@users.noreply.github.com>
2022-01-31 15:32:09 +0100

    feat(change-layout): fix current layout fetching

--- change_layout_wayland.sh
+++ change_layout_wayland.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 swaymsg input "1:1:AT_Translated_Set_2_keyboard" xkb_switch_layout next
-LAYOUT=$(swaymsg -t get_inputs | jq '.[0].xkb_active_layout_name')
+LAYOUT=$(swaymsg -t get_inputs | jq '.[] | select(.identifier == "1:1:AT_Translated_Set_2_keyboard") | .xkb_active_layout_name')
 notify-send -t 1000 "$LAYOUT" -h string:x-canonical-private-synchronous:change_layout_wayland
