80f01ea475ddf9885f1bd290141e9a46d7559ea2
dbeley <6568955+dbeley@users.noreply.github.com>
2022-12-04 23:37:24 +0100

    Update scripts

--- dwmstatus.sh
+++ dwmstatus.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 while true; do
 	# add 2 seconds to script runtime
-	NETWORK_UP="$(vnstat -tr 2 | grep tx | xargs | cut -d ' ' -f2-3)"
+	# NETWORK_UP="$(vnstat -tr 2 | grep tx | xargs | cut -d ' ' -f2-3)"
 	# add 2 seconds to script runtime
-	NETWORK_DOWN="$(vnstat -tr 2 | grep rx | xargs | cut -d ' ' -f2-3)"
+	# NETWORK_DOWN="$(vnstat -tr 2 | grep rx | xargs | cut -d ' ' -f2-3)"
 	FREQ="$(grep MHz /proc/cpuinfo | head -n 1 | cut -d ' ' -f3 | cut -d '.' -f1)"
-	CPU="$(($(cat /sys/class/hwmon/hwmon0/device/temp) /1000))°C"
+	CPU="$(($(cat /sys/class/hwmon/hwmon1/device/temp) /1000))°C"
 	MEM=$(free | awk '/Mem/{printf("%.2f%"), $3/$2*100}')
 	LOAD=$(cut -d " " -f1-3 /proc/loadavg)
 	VOL=$(pamixer --get-volume-human)
-	DATE="$(date)"
+	BAT=$(cat /sys/class/power_supply/BAT0/capacity)
+	DATE="$(date +%H:%M:%S)"
+ # DOWN $NETWORK_DOWN\
+ # | UP $NETWORK_UP\
 	OUTPUT="\
- DOWN $NETWORK_DOWN\
- | UP $NETWORK_UP\
- | LOAD $LOAD\
+ LOAD $LOAD\
  | CPU $CPU\
  | FREQ ${FREQ}MHz\
  | MEM $MEM\
  | VOL $VOL\
+ | BAT $BAT%\
  | $DATE"
  	# echo $OUTPUT
 	xsetroot -name "$OUTPUT"
-	sleep 1
+	sleep 4
 done
--- maim_cropped.sh
+++ maim_cropped.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-maim -s -o "$HOME"/Nextcloud/07_Images/Captures\ d\'écran/"$(date +%s)"_cropped.png
+maim -s -o "$HOME"/Nextcloud/07_Images/02_Captures-d-écran/"$(date +%s)"_cropped.png
