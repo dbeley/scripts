06aeefa767687ad967cd410d131d3c173c78b04f
dbeley <6568955+dbeley@users.noreply.github.com>
2022-12-10 11:23:44 +0100

    Update dwmstatus.sh

--- dwmstatus.sh
+++ dwmstatus.sh
@@ -4,19 +4,19 @@ while true; do
 	# NETWORK_UP="$(vnstat -tr 2 | grep tx | xargs | cut -d ' ' -f2-3)"
 	# add 2 seconds to script runtime
 	# NETWORK_DOWN="$(vnstat -tr 2 | grep rx | xargs | cut -d ' ' -f2-3)"
-	FREQ="$(grep MHz /proc/cpuinfo | head -n 1 | cut -d ' ' -f3 | cut -d '.' -f1)"
+	# FREQ="$(grep MHz /proc/cpuinfo | head -n 1 | cut -d ' ' -f3 | cut -d '.' -f1)"
 	CPU="$(($(cat /sys/class/hwmon/hwmon1/device/temp) /1000))°C"
 	MEM=$(free | awk '/Mem/{printf("%.2f%"), $3/$2*100}')
 	LOAD=$(cut -d " " -f1-3 /proc/loadavg)
 	VOL=$(pamixer --get-volume-human)
 	BAT=$(cat /sys/class/power_supply/BAT0/capacity)
-	DATE="$(date +%H:%M:%S)"
+	DATE="$(date +'%Y/%m/%d %H:%M:%S')"
  # DOWN $NETWORK_DOWN\
  # | UP $NETWORK_UP\
+ # | FREQ ${FREQ}MHz\
 	OUTPUT="\
  LOAD $LOAD\
  | CPU $CPU\
- | FREQ ${FREQ}MHz\
  | MEM $MEM\
  | VOL $VOL\
  | BAT $BAT%\
